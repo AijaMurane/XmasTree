85a84e3f1bf669978f42dfa6a87fb36ca8c63b64
Aija Murane <aija.murane@gmail.com>
2020-08-17 21:21:06 +0300

    Second commit

--- src/main/scala/XmasTree.scala
+++ src/main/scala/XmasTree.scala
@@ -2,13 +2,13 @@ import scala.io.StdIn.readLine
 
 object XmasTree extends App {
   println("Please enter a natural number that is greater than 3!")
-  var i = readLine()
-  var j = if (!i.isEmpty) i.toInt else args(0).toInt
-  var a = 1
+  val i = readLine()
+  var j = if (!i.isEmpty) i.toInt else 3
+  var temp = 1
 
-  while (j>0) {
-    println(" "*(j-1) + "*"*a)
-    j -= 1
-    a += 2
+  for (count <- 1 to j) {
+    println(" "*(j-count) + "*"*temp)
+    temp += 2
   }
+
 }
